6fe0bdacb8dda292b58edb69dd4eec236f368d28
Chansoo Park <chansoopark@Chansoos-MacBook-Air.local>
2024-04-23 01:55:17 +0900

    Create Entities and set db migrations and seeding

---
+++ src/apis/auth/entity/userAuth.entity.ts
@@ -0,0 +1,29 @@
+import { User } from 'src/apis/user/entity/user.entity';
+import {
+  Column,
+  CreateDateColumn,
+  Entity,
+  JoinColumn,
+  OneToOne,
+  PrimaryGeneratedColumn,
+  UpdateDateColumn,
+} from 'typeorm';
+
+@Entity()
+export class UserAuth {
+  @PrimaryGeneratedColumn('uuid')
+  id: string;
+
+  @OneToOne(() => User, { onDelete: 'CASCADE' })
+  @JoinColumn({ name: 'user_id' })
+  user: User;
+
+  @Column({ name: 'auth_code', type: 'int', width: 6, nullable: false })
+  authCode: number;
+
+  @CreateDateColumn({ name: 'created_at' })
+  createdAt: Date;
+
+  @UpdateDateColumn({ name: 'updated_at' })
+  updatedAt: Date;
+}
